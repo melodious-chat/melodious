62cf958d5156f2d1d06673ac32a41aaf9568bfb5
Nickolay Ilyushin <nickolay02@inbox.ru>
2019-01-30 18:52:49 +0200

    .

--- database.go
+++ database.go
@@ -12,18 +12,31 @@ type Database struct {
 	db  *sql.DB
 }
 
+// RegisterUser - adds a new user to the database
+func (db *Database) RegisterUser(name string, passhash string) error {
+	//sum := sha256.Sum256([]byte(passhash))
+	//sumstr := string(sum[:32])
+
+	return nil
+}
+
 // NewDatabase - creates a new Database instance
 func NewDatabase(mel *Melodious, addr string) (*Database, error) {
 	db, err := sql.Open("postgres", addr)
 
-	_, err = db.Exec("CREATE TABLE IF NOT EXISTS accounts ( id serial PRIMARY KEY, username varchar(32), passhash varchar(64) );")
+	_, err = db.Exec(`
+		CREATE TABLE IF NOT EXISTS accounts (
+			id serial PRIMARY KEY,
+			username varchar(32) UNIQUE,
+			passhash varchar(64)
+		);`)
 	if err != nil {
 		return nil, err
 	}
 
-	if err != nil {
-		return nil, err
-	}
+	//if err != nil {
+	//	return nil, err
+	//}
 
 	return &Database{
 		mel: mel,
